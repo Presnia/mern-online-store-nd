30544661b1858af47b96136c74cfc270ff25edfe
Andrei Dzebysh <presnia@yahoo.com>
2022-07-17 20:31:47 +0300

    Functionality of user authorization is implemented

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -47,12 +47,9 @@ class UserController {
         return res.json({token});
     }
 
-    async check(req, res, next) {
-        const {id} = req.query;
-        if (!id) {
-            return next(ApiError.badRequest('ID not set'));
-        }
-        res.json(id);
+    async check(req, res) {
+        const token = generateJwt(req.user.id, req.user.email, req.user.role);
+        res.json({token});
     }
 }
 
