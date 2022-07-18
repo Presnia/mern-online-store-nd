eb9e6ff2c657626a9127baa75b60b5d14cc45456
Andrei Dzebysh <presnia@yahoo.com>
2022-07-18 20:15:58 +0300

    The Auth page and its functionality is implemented

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React, {useContext} from 'react';
+import {Context} from "../index";
+import {Button, Container, Nav, Navbar, NavLink} from "react-bootstrap";
+import {SHOP_ROUTE} from "../utils/consts";
+import {observer} from "mobx-react-lite";
+
+const NavBar = observer(() => {
+    const {user} = useContext(Context);
+
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <NavLink to={SHOP_ROUTE} style={{color: 'white'}}>BuyDevice</NavLink>
+                {user.isAuth ?
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button variant={'outline-light'}>Admin Panel</Button>
+                        <Button className="ml-3" variant={'outline-light'}>Login</Button>
+                    </Nav>
+                    :
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button
+                            variant={'outline-light'}
+                            onClick={() => user.setIsAuth(true)}
+                        >
+                            Authorization
+                        </Button>
+                    </Nav>
+                }
+            </Container>
+        </Navbar>
+    );
+});
+
+export default NavBar;
