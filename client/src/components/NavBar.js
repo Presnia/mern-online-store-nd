fa22d6542bcc299882db366540306f9e5c20bdbf
Andrei Dzebysh <presnia@yahoo.com>
2022-07-20 09:50:51 +0300

    The AppRouter and NavBar components have been adjusted

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React, {useContext} from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context);
+    const history = useHistory();
 
     return (
         <Navbar bg="dark" variant="dark">
@@ -19,8 +20,18 @@ const NavBar = observer(() => {
                 </NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={'outline-light'}>Admin Panel</Button>
-                        <Button className="ml-3" variant={'outline-light'}>Login</Button>
+                        <Button
+                            variant={'outline-light'}
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Admin Panel
+                        </Button>
+                        <Button
+                            variant={'outline-light'}
+                            onClick={() => history.push(LOGIN_ROUTE)} className="ml-3"
+                        >
+                            Logout
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: 'white'}}>
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Button, Container} from "react-bootstrap";
 
 const Admin = () => {
     return (
-        <div>
-            ADMIN
-        </div>
+        <Container className="d-flex flex-column">
+            <Button>Add New Type</Button>
+            <Button>Add New Brand</Button>
+            <Button>Add New Device</Button>
+        </Container>
     );
 };
 
